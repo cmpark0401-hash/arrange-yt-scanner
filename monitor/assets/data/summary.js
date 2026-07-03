8decb7406060f638224b8b8157a0b8035dc6d7a8
cmpark0401-hash <cm.park0401@gmail.com>
2026-07-03 18:12:49 +0900

    manual: 2026-07-03 18:12 폭발 1건 감지 (마감시황 25.8K뷰)

--- monitor/assets/data/summary.js
+++ monitor/assets/data/summary.js
@@ -1,1 +1,1 @@
-window.__SUMMARY__ = {"channels": 54, "hot_videos": 50, "recent_alerts": 1, "updated_at": "2026-07-03 16:27"};
+window.__SUMMARY__ = {"channels": 54, "hot_videos": 50, "recent_alerts": 1, "updated_at": "2026-07-03 18:12"};
