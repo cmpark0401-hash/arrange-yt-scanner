dc76e9ed2e9faf24a1ee0ca6c04d73c8e16ce647
cmpark0401-hash <cm.park0401@gmail.com>
2026-07-05 18:17:10 +0900

    manual: 2026-07-05 18:17 KST 골든타임 갱신 (폭발 7건 감지)

--- monitor/assets/data/summary.js
+++ monitor/assets/data/summary.js
@@ -1,1 +1,1 @@
-window.__SUMMARY__ = {"channels": 54, "hot_videos": 0, "recent_alerts": 1, "updated_at": "2026-07-05 06:26"};
+window.__SUMMARY__ = {"channels": 54, "hot_videos": 50, "recent_alerts": 7, "updated_at": "2026-07-05 18:17"};
