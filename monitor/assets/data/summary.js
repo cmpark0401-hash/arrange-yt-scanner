18b395d307bb724573cb6900b6e5d4254e3469d1
cmpark0401-hash <cm.park0401@gmail.com>
2026-07-14 15:29:27 +0900

    site: 65채널 SS 14개 데이터 반영 (재빌드)

--- monitor/assets/data/summary.js
+++ monitor/assets/data/summary.js
@@ -1,1 +1,1 @@
-window.__SUMMARY__ = {"channels": 56, "hot_videos": 0, "recent_alerts": 1, "updated_at": "2026-07-14 05:18"};
+window.__SUMMARY__ = {"channels": 65, "hot_videos": 0, "recent_alerts": 1, "updated_at": "2026-07-14 15:29"};
