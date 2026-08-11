94af994d3c6b48b454ec65208bbb9ab959b7e6ed
cmpark0401-hash <cm.park0401@gmail.com>
2026-08-11 16:41:49 +0900

    auto: full refresh 2026-08-11 16:41

--- monitor/assets/data/summary.js
+++ monitor/assets/data/summary.js
@@ -1,1 +1,1 @@
-window.__SUMMARY__ = {"channels": 78, "hot_videos": 0, "recent_alerts": 8, "updated_at": "2026-08-11 16:34"};
+window.__SUMMARY__ = {"channels": 85, "hot_videos": 0, "recent_alerts": 8, "updated_at": "2026-08-11 16:41"};
